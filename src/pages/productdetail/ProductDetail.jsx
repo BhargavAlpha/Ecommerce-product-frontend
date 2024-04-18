12a065a643599d2ba18cf3be8ee23941bbe0b678
Bhargav <bhargavvenkat515@gmail.com>
2024-04-18 20:20:39 +0530

    Vercel error resolved

--- src/pages/productdetail/ProductDetail.jsx
+++ src/pages/productdetail/ProductDetail.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import styles from './ProductDetail.module.css';
 import Navbar from '../../components/navbar/Navbar';
-import { set } from 'mongoose';
 
 const ProductDetail = () => {
   const url = import.meta.env.VITE_BACKEND_URL;
